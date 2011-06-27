00821f72a4a15b64bb6bc841dc259c36c251c015
Akiko Saito <akiko74@yahoo.co.jp>
2011-06-27 22:10:03 +0900

    本番環境への移行ファイルが完成。

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,9 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+set :rvm_ruby_string, '1.8.7'
+set :rvm_type, :user
 set :application, "sakana"
 set :repository,  "ssh://sakanakana.info:7022/home/a-saito/sakana/.git"
 
@@ -9,6 +15,7 @@ set :use_sudo, false
 set :user, "a-saito"
 set :port, 7022
 set :default_run_options, :pty => true
+#set :rake, "/opt/local/bin/rake"
 
 role :web, "sakanakana.info"      # Your HTTP server, Apache/etc
 role :app, "sakanakana.info"      # This may be the same as your `Web` server
@@ -20,9 +27,15 @@ role :db,  "sakanakana.info", :primary => true # This is where Rails migrations 
 
 # If you are using Passenger mod_rails uncomment this:
   namespace :deploy do
-    task :start do ; end
-    task :stop do ; end
-    task :restart, :roles => :app, :except => { :no_release => true } do
-      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    task :start, :roles => :app do
+       run "touch #{current_release}/tmp/restart.txt"
+    end
+    
+    task :stop, :roles => :app do
+    end
+    
+    desc "Restart Application"
+    task :restart, :roles => :app do
+      run "touch #{current_release}/tmp/restart.txt"
     end
   end
