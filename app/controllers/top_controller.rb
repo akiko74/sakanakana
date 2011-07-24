dbb8ef90b8a15c3d7db0909380932d275c57a433
Akiko Saito <akiko74@yahoo.co.jp>
2011-07-24 17:52:37 +0900

    トップページの写真をランダム表示に変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @pictures = Picture.order('created_at desc').limit(6)
+    @pictures = Picture.order('RAND()').limit(6)
     @infos = Info.order('created_at desc').limit(3)
   end
 
