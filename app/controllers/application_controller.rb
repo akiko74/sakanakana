7514ca1d88b5edc390797a84dbde9c43521d7eb6
Akiko Saito <akiko74@yahoo.co.jp>
2011-07-31 19:44:53 +0900

    オススメ写真へのリンクをshowに変更。登録がないときはリンクが出ないようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_admin!, :except => [ :index, :show, :movies, :equipments, :contact ]
 
-
-  def title
-    @title = "タオ島、他ダイビングスポットで出会った魚たち"
-  end
-
 end
