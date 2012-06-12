a44a4b5ec353d5cca12650f334a6ab06055f6913
a-saito <a-saito@www10084ue.sakura.ne.jp>
2012-06-12 12:41:27 +0900

    modified according to construct new server

--- Gemfile
+++ Gemfile
@@ -5,11 +5,11 @@ gem 'rails', '3.0.0'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'mysql2'
-gem 'ruby-debug',:require=>nil
+gem 'mysql2', "~> 0.2.0"
+#gem 'ruby-debug',:require=>nil
 
 #画像縮小用
-gem 'paperclip', :git => 'git://github.com/thoughtbot/paperclip.git'
+gem 'paperclip', '2.7.0'
 
 #Twitter表示用
 gem 'twitter'
@@ -21,7 +21,7 @@ gem 'exifr', :git => 'git://github.com/remvee/exifr.git'
 gem 'jquery-rails'
 
 #devise認証用
-gem 'devise', :git => 'git://github.com/plataformatec/devise.git'
+gem 'devise', '1.2.1'
 
 gem "will_paginate", "~> 3.0.pre2"
 
@@ -33,6 +33,7 @@ gem 'fastercsv'
 
 # Deploy with Capistrano
 gem 'capistrano'
+gem 'rvm-capistrano'
 
 #Deploy management
 gem 'passenger' 
@@ -53,6 +54,12 @@ gem 'passenger'
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
-# group :development, :test do
-#   gem 'webrat'
-# end
+#group :development, :test do
+#   gem 'capybara'
+#   gem 'rspec'
+#   gem 'ruby-debug19', :require => 'ruby-debug'
+#   gem 'cucumber-rails'
+#   gem 'cucumber-rails-training-wheels'
+#   gem 'database_cleaner'
+#   gem 'rspec-rails'
+#end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 require "bundler/capistrano"
 
-set :rvm_ruby_string, '1.8.7'
+set :rvm_ruby_string, 'ruby-1.8.7-p358@sakana'
 set :rvm_type, :user
 set :application, "sakana"
-set :repository,  "ssh://sakanakana.info:7022/home/a-saito/sakana/.git"
+set :repository,  "git://github.com/akiko74/sakanakana.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -17,11 +17,20 @@ set :port, 7022
 set :default_run_options, :pty => true
 #set :rake, "/opt/local/bin/rake"
 
-role :web, "sakanakana.info"      # Your HTTP server, Apache/etc
-role :app, "sakanakana.info"      # This may be the same as your `Web` server
-role :db,  "sakanakana.info", :primary => true # This is where Rails migrations will run
+role :web, "49.212.146.98"      # Your HTTP server, Apache/etc
+role :app, "49.212.146.98"      # This may be the same as your `Web` server
+role :db,  "49.212.146.98", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
+set :default_environment, {
+#  'PATH' => "/home/#{user}/.rvm/gems/ruby-1.8.7-p358/bin:/home/#{user}/.rvm/gems/ruby-1.8.7-p358@sakana/bin:/home/#{user}/.rvm/rubies/ruby-1.8.7-p358/bin:/home/#{user}/.rvm/bin:$PATH",
+#'RUBY_VERSION' => 'ruby 1.8.7',
+#'GEM_HOME' => "/home/#{user}/.rvm/gems/ruby-1.8.7-p358@sakana",
+#'GEM_PATH' => "/home/#{user}/.rvm/gems/ruby-1.8.7-p358@sakana:/home/#{user}/.rvm/gems/ruby-1.8.7-p358@global",
+
+'BUNDLE_PATH' => '/home/a-saito/.rvm/gems/ruby-1.8.7-p358@sakana' # If you are using bundler.
+}
+#
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
