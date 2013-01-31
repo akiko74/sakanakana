c893177334aa8444b93dde393d72594c87527013
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-01-31 10:38:05 +0900

    Update migration files

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,21 @@
 ActiveRecord::Schema.define(:version => 20110725145201) do
 
   create_table "admins", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",                     :default => 0
+    t.integer  "failed_attempts",        :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
@@ -36,8 +37,8 @@ ActiveRecord::Schema.define(:version => 20110725145201) do
 
   create_table "areas", :force => true do |t|
     t.string   "areaname",   :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "details", :force => true do |t|
@@ -46,8 +47,8 @@ ActiveRecord::Schema.define(:version => 20110725145201) do
     t.string   "othername"
     t.text     "description"
     t.integer  "genre_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "details_pictures", :id => false, :force => true do |t|
@@ -57,8 +58,8 @@ ActiveRecord::Schema.define(:version => 20110725145201) do
 
   create_table "divelogs", :force => true do |t|
     t.integer  "divesite_id", :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.datetime "start_date",  :null => false
     t.datetime "end_date",    :null => false
   end
@@ -66,8 +67,8 @@ ActiveRecord::Schema.define(:version => 20110725145201) do
   create_table "divesites", :force => true do |t|
     t.string   "pointname",            :null => false
     t.integer  "area_id",              :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
     t.string   "sitemap_file_name"
     t.string   "sitemap_content_type"
     t.integer  "sitemap_file_size"
@@ -79,28 +80,28 @@ ActiveRecord::Schema.define(:version => 20110725145201) do
     t.string   "camera_model"
     t.datetime "shot_date_time"
     t.integer  "picture_id",     :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
   create_table "genres", :force => true do |t|
     t.string   "genrename",  :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "infos", :force => true do |t|
     t.string   "content"
     t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "pickups", :force => true do |t|
     t.integer  "picture"
     t.string   "comment"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "pictures", :force => true do |t|
@@ -110,8 +111,8 @@ ActiveRecord::Schema.define(:version => 20110725145201) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
   create_table "pictures_tags", :id => false, :force => true do |t|
@@ -121,15 +122,8 @@ ActiveRecord::Schema.define(:version => 20110725145201) do
 
   create_table "tags", :force => true do |t|
     t.string   "tagname",    :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "whatsnews", :force => true do |t|
-    t.string   "content"
-    t.string   "link"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
