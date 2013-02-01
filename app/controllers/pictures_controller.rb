2f33efff4e72d5c52c4fbeda2f8a244bde95715b
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-02-01 20:37:34 +0900

    Add transaction to create pictures.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -52,20 +52,24 @@ class PicturesController < ApplicationController
   
   # POST /pictures.xml
   def create
+    ActiveRecord::Base.transaction do
     @picture = Picture.create(params[:picture])
 
     respond_to do |format|
       if @picture.save
-      @divelog = Divelog.order('start_date DESC').where('start_date < ? and end_date > ?', @picture.exif.shot_date_time, @picture.exif.shot_date_time).first
+        unless @divelog = Divelog.order('start_date DESC').where('start_date < ? and end_date > ?', @picture.exif.shot_date_time, @picture.exif.shot_date_time).first
+          raise "#{Divelog.model_name.human} is not found..."
+        end
       @picture.divelog_id = @divelog.id
       @picture.divesite_id = @divelog.divesite_id
-      @picture.update_attributes(:divesite_id)
+      @picture.save
         format.html { redirect_to(@picture, :notice => 'Picture was successfully created.') }
         format.xml  { render :xml => @picture, :status => :created, :location => @picture }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @picture.errors, :status => :unprocessable_entity }
       end
+    end
     end
   end
 
