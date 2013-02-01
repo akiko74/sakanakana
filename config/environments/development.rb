4d18510f340b26daca26c1acc2dda62d61609c22
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-02-01 12:54:16 +0900

    Remove unused option

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,6 @@ Sakana::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
-  Paperclip.options[:command_path] = "/usr/bin"
 
   # Do not compress assets
   config.assets.compress = false
